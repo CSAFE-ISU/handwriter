e0c5acd5b3c243d45f710960ca7f81116c02d211
NICHOLAS BERRY <Nick@NICHOLASs-MacBook-Pro-4.local>
2018-03-07 20:57:48 -0600

    Remove dependency on imager & rewrote threshold/grayscale + rgba2rgb in C++. Plot faster via NA colors and alpha.

--- src/ThinImageCpp.cpp
+++ src/ThinImageCpp.cpp
@@ -7,6 +7,12 @@ using namespace arma;
 //
 // [[Rcpp::depends(RcppArmadillo)]]
 
+arma::mat rgba2rgb(arma::mat img);
+arma::uvec whichToFill(arma::mat img);
+void countBNeighbors_7or8(const arma::mat &img, uvec white, uvec &toBlack);
+arma::mat rgb2grayscale(arma::cube img);
+
+arma::uvec thinImage(arma::mat img);
 void countBNeighbors(const arma::mat &img, uvec checkList, vec &toWhite);
 void countWBTransitions(const arma::mat &img, uvec checkList, vec &toWhite);
 void count246(const arma::mat &img, uvec checkList, vec &toWhite);
@@ -14,6 +20,100 @@ void count468(const arma::mat &img, uvec checkList, vec &toWhite);
 void count248(const arma::mat &img, uvec checkList, vec &toWhite);
 void count268(const arma::mat &img, uvec checkList, vec &toWhite);
 
+//' rgba2rgb
+//' 
+//' Removes alpha channel from png image.
+//'
+//' @param img A 3-d array with slices R, G, B, and alpha.
+//' @export
+// [[Rcpp::export]]
+arma::cube rgba2rgb(arma::cube img)
+{
+  bool realAlphaFlag = arma::any(vectorise(img.slice(3)) < 1);
+  
+  if(realAlphaFlag)
+  {
+    for(int i = 0; i < img.n_rows; i++)
+    {
+      for(int j = 0; j < img.n_cols; j++)
+      {
+        img.at(i, j, 0) = img.at(i,j,0)*img.at(i,j,3);
+        img.at(i, j, 1) = img.at(i,j,1)*img.at(i,j,3);
+        img.at(i, j, 2) = img.at(i,j,2)*img.at(i,j,3);
+      }
+    }
+  }
+  
+  return(img.slices(0,2));
+}
+
+//' rgba2rgb
+//' 
+//' Removes alpha channel from png image.
+//'
+//' @param img A 3-d array with slices R, G, B, and alpha.
+//' @export
+// [[Rcpp::export]]
+arma::mat rgb2grayscale(arma::cube img)
+{
+  arma::mat grayImg(img.n_rows, img.n_cols, fill::ones);
+  
+  for(int i = 0; i < img.n_rows; i++)
+  {
+    for(int j = 0; j < img.n_cols; j++)
+    {
+      grayImg.at(i, j) = 0.299*img.at(i,j,0) + 0.587*img.at(i,j,1) + 0.114*img.at(i,j,2);
+    }
+  }
+  
+  return(grayImg);
+}
+
+//' whichToFill
+//' 
+//' Finds pixels in the plot that shouldn't be white and makes them black. Quick and helpful cleaning for before the thinning algorithm runs.
+//'
+//' @param img A binary matrix.
+//' @export
+// [[Rcpp::export]]
+arma::uvec whichToFill(arma::mat img)
+{
+  int n = img.n_rows;
+  int p = img.n_cols;
+  uvec flag(img.n_elem);
+  for(int i = 0; i < img.n_elem; i++)
+  {
+    flag[i] = (img[i] == 1 && !(i % n == 0 || i % n == n-1 || i / n == 0 || i / n == p-1));
+  }
+  
+  uvec white = find(flag);
+  uvec toBlack = zeros<uvec>(white.n_elem);
+  
+  countBNeighbors_7or8(img, white, toBlack);
+  
+  return(white.elem(find(toBlack == 1)) + 1);
+}
+
+void countBNeighbors_7or8(const arma::mat &img, uvec white, uvec &toBlack)
+{
+  vec neighborInd = zeros<vec>(8);
+  int n = img.n_rows;
+  int cell;
+  int numBlack = 0;
+  uvec isBlack;
+  for(int i = 0; i < white.n_elem; i++)
+  {
+    cell = white[i];
+    neighborInd << img[cell - 1] << img[cell + n - 1] << img[cell + n] << img[cell + n + 1] << img[cell + 1] 
+                << img[cell - n + 1] << img[cell - n] << img[cell - n - 1];
+    isBlack = find(neighborInd == 0);
+    numBlack = isBlack.n_elem;
+    if(numBlack >= 7)
+    {
+      toBlack[i] = 1;
+    }
+  }
+}
 //' thinImage
 //' 
 //' This function returns a vector of locations for black pixels in the thinned image.
@@ -188,11 +288,18 @@ void countWBTransitions(const arma::mat &img, uvec checkList, vec &toWhite)
 // run after the compilation.
 //
 /*** R
-data(message)
-img = message
-message = list()
-message$image = crop(img)
-message$thin = thinImage(message$image)
-plotImageThinned(message$image, message$thin)
+#data(message)
+#img = message
+#message = list()
+#message$image = crop(img)
+#message$thin = thinImage(message$image)
+#plotImageThinned(message$image, message$thin)
+
+test = png::readPNG("/Users/Nick/Documents/Projects/CSAFE/Handwriting/data/Writing_csafe_all.png")
+dim(test)
+oldTest = test[,,1:3]
+test = rgba2rgb(test)
+dim(test)
+all(test[,,1:3] == oldTest[,,1:3])
 */
 
