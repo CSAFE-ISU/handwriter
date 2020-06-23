601ae456097a256d38dd8bdd2b12b3184644ea26
James Taylor <Jamesetay1@gmail.com>
2020-06-23 16:16:20 -0500

    i/j

--- R/ExtractWordInfo.R
+++ R/ExtractWordInfo.R
@@ -74,7 +74,7 @@ find_colorpoints = function(words, dims){
   if(has_i_or_j){
     yellowrow = mean(colordf[,2])
     yellowcol = mean(colordf[,3])
-    yellowindex = rc_to_i(yellowrow, yellowcol, dims)
+    yellowindex = rc_to_i(yellowrow, yellowcol-10, dims)
     yellow_point <- data.frame(yellowindex, i_to_rc(yellowindex, dims), 6, "yellow")
     colnames(yellow_point) <- c("point", "row", "col", "colorIndex", "color")
     colordf <- rbind(colordf, yellow_point)
