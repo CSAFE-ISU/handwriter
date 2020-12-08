53148e8ff54e77985f9dab837c071970d7976493
James Taylor <Jamesetay1@gmail.com>
2020-12-08 17:32:45 -0600

    Update WordModelMaker.R

--- R/WordModelMaker.R
+++ R/WordModelMaker.R
@@ -6,26 +6,23 @@ library(data.table)
 AsJSON <- fromJSON(file = "data/2L1W.json")
 
 #Create a new DF and fill it up from each character entry
-dataDF <- data.frame(original_height=numeric(0),original_width=numeric(0),height=numeric(0),width=numeric(0),x=numeric(0),y=numeric(0),label=character(0))
-for (i in length(AsJSON)){
-  for(char in AsJSON[[i]][["completions"]][[1]][["result"]])
+dataDF <- data.frame(line=numeric(0),original_height=numeric(0),original_width=numeric(0),height=numeric(0),width=numeric(0),x=numeric(0),y=numeric(0),label=character(0))
+for (i in 1:length(AsJSON)){
+  for(char in AsJSON[[i]][["completions"]][[1]][["result"]]){
     dataDF[nrow(dataDF) + 1,] =
-    list(
+    list(i,
     char$original_height,char$original_width,
     char$value$height,char$value$width,
     char$value$x,char$value$y,
     char$value$rectanglelabels)
+  }
 }
 
-
-print(nrow(dataDF))
-#Use function to convert to Nice proportional list objects
-
-
-#Sort the fuction by x so letters are in order
+#Sort DF By first line number, and then x value
+dataDF=dataDF[order( dataDF[,1], dataDF[,6] ),]
 
 
-#Get info based on word dist from left and right
+#Map to function to do line by line math and get ratios
 
 
 #Put into DF for model training
