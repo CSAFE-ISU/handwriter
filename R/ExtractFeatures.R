4cc745fb05509ec14a0f9103eb0d4718662a1c01
James Taylor <Jamesetay1@gmail.com>
2021-05-19 12:03:07 -0500

    Docs updates

--- R/ExtractFeatures.R
+++ R/ExtractFeatures.R
@@ -2,10 +2,6 @@
 #'
 #' Primary driver of feature extraction. 
 #' Parses all characters from a processed image.
-#' For information detailing each feature, please see
-#' ?get_aspect_info
-#' ?get_centroid_info
-#' ?get_loop_info
 #' @param character_lists Output from processHandwriting$letterLists
 #' @param img_dim Dimensions of binary image
 #' @keywords centroid, skew, slant, lean, character
@@ -104,7 +100,6 @@ i_to_rci = function(nodes, dims, fixed = FALSE)
   colnames(rowcolmatrix) = c('y','x','index')
   return(rowcolmatrix)
 }
-
 
 #' rc_to_i
 #'
@@ -407,55 +402,56 @@ add_word_info = function(letterList, dims){
   return(letterList)
 }
 
-#' #' add_word_info_old
-#' #' OLD, CURRENT VERSION ABOVE
-#' #' Associates characters to their respective word numbers by distance between right edge of char and left edge of next
-#' #' Needs improvement if runtime becomes a problem
-#' #' @param character_features All extracted features 
-#' #' @param img_dim Dimensions of binary image
-#' #' @keywords character, features, line number
-#' #' @return Appends line information to character features
-#' #' @export
-#' add_word_info_old = function(letterList, dims){#character_features){
-#'   
-#'   #loop that goes through and records distances between rightmost_col and leftmost_col of next
-#'   dist_between_list = list()
-#'   right_col = letterList[[1]]$characterFeatures$rightmost_col
-#'   for(i in 2:length(letterList)){
-#'     left_col = letterList[[i]]$characterFeatures$leftmost_col
-#'     dist_between = left_col - right_col
-#'     dist_between_list <- append(dist_between_list, dist_between)
-#'     right_col = letterList[[i]]$characterFeatures$rightmost_col                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-#'   }    
-#'   dist_between_vec <- unlist(dist_between_list)
-#'   new_line_threshold = -(dims[2]/2)
-#'   
-#'   
-#'   #find the average of these measurements - here a few ideas on how to calcualte it
-#'   dist_between_vec <- append(dist_between_vec, c(0))
-#'   
-#'   #1: ZERO OUT, TAKE MEAN * 1.5
-#'   dist_vec_zeroed <- dist_between_vec #save off a zeroed one to find our threshold, keep the real one for processing
-#'   dist_vec_zeroed[dist_vec_zeroed < 0] <- 0
-#'   dist_between_mean = mean(dist_vec_zeroed)
-#'   splitThreshold = dist_between_mean * 1.5
-#'   
-#'   #split up the words according to this measurement
-#'   wordCount = 1
-#'   for(i in 1:(length(letterList))){
-#'     letterList[[i]]$characterFeatures = c(letterList[[i]]$characterFeatures, list(wordIndex = wordCount))
-#'     
-#'     if(dist_between_vec[[i]] < new_line_threshold){ #CONSULT THE MODEL HERE
-#'       wordCount = wordCount + 1
-#'     }
-#'     
-#'     if(dist_between_vec[[i]] >= splitThreshold){
-#'       wordCount = wordCount + 1
-#'     }
-#'   }
-#'   
-#'   return(letterList)
-#' }
+# #' add_word_info_old
+# #' OLD, CURRENT VERSION ABOVE
+# #' Associates characters to their respective word numbers by distance between right edge of char and left edge of next
+# #' Needs improvement if runtime becomes a problem
+# #' @param character_features All extracted features
+# #' @param img_dim Dimensions of binary image
+# #' @keywords character, features, line number
+# #' @return Appends line information to character features
+# #' @export
+
+# add_word_info_old = function(letterList, dims){#character_features){
+#   
+#   #loop that goes through and records distances between rightmost_col and leftmost_col of next
+#   dist_between_list = list()
+#   right_col = letterList[[1]]$characterFeatures$rightmost_col
+#   for(i in 2:length(letterList)){
+#     left_col = letterList[[i]]$characterFeatures$leftmost_col
+#     dist_between = left_col - right_col
+#     dist_between_list <- append(dist_between_list, dist_between)
+#     right_col = letterList[[i]]$characterFeatures$rightmost_col                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+#   }    
+#   dist_between_vec <- unlist(dist_between_list)
+#   new_line_threshold = -(dims[2]/2)
+#   
+#   
+#   #find the average of these measurements - here a few ideas on how to calcualte it
+#   dist_between_vec <- append(dist_between_vec, c(0))
+#   
+#   #1: ZERO OUT, TAKE MEAN * 1.5
+#   dist_vec_zeroed <- dist_between_vec #save off a zeroed one to find our threshold, keep the real one for processing
+#   dist_vec_zeroed[dist_vec_zeroed < 0] <- 0
+#   dist_between_mean = mean(dist_vec_zeroed)
+#   splitThreshold = dist_between_mean * 1.5
+#   
+#   #split up the words according to this measurement
+#  wordCount = 1
+#   for(i in 1:(length(letterList))){
+#     letterList[[i]]$characterFeatures = c(letterList[[i]]$characterFeatures, list(wordIndex = wordCount))
+#    
+#     if(dist_between_vec[[i]] < new_line_threshold){ #CONSULT THE MODEL HERE
+#       wordCount = wordCount + 1
+#     }
+#     
+#     if(dist_between_vec[[i]] >= splitThreshold){
+#       wordCount = wordCount + 1
+#     }
+#   }
+#   
+#   return(letterList)
+# }
 
 #' get_loop_info
 #'
@@ -472,7 +468,6 @@ add_word_info = function(letterList, dims){
 #' @keywords character, loop, associate
 #' @return Loop information to respective character
 #' @export
-
 get_loop_info = function(character,img_dim){
   
   #loops = loop_extract(character$allPaths)
