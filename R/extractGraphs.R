c52d145f6cec0c2dd109cf058b453237ec83cc3e
felbasa <felixbaez822@gmail.com>
2022-04-14 13:12:34 -0500

    added extractGraphs.R

---
+++ R/extractGraphs.R
@@ -0,0 +1,64 @@
+#' Extracts graphs from a .png image using handwriter functions
+#' 
+#' extractGraphs
+#' 
+#' @param image
+#' @param source_folder
+#' @param save_folder
+#' @return saves proclist to an rds file
+getGraphs = function(image, source_folder = getwd(), save_folder = getwd()){
+  setwd(source_folder)
+  doc = proclist = list() 
+  doc$image = readPNGBinary(image)
+  doc$thin = thinImage(doc$image)
+  proclist[["process"]] = processHandwriting(doc$thin, dim(doc$image))
+  proclist[["docname"]] = substr(basename(image), start = 0, stop = nchar(basename(image))-4)
+  proclist[["thin"]] = doc$thin
+  proclist[["image"]] = doc$image
+  
+  setwd(save_folder)  
+  saveRDS(object = proclist, file = paste0(save_folder, "/", substr(basename(image), start = 0, stop = nchar(basename(image))-4), "_proclist.rds"))
+  gc()
+}
+
+
+#' Extracts graphs from .png images and saves each by their respective writer
+#' 
+#' extractGraphs
+#' 
+#' @importFrom foreach foreach
+#' @importFrom doParallel registerDoParallel
+#' @import handwriter
+#' @import purrr
+#' 
+#' @param source_folder path to folder containing .png images
+#' @param save_folder path to folder where graphs are saved to
+#' @return saves graphs in an rds file
+#' @examples 
+#' sof = "path to folder containing .png images"
+#' saf = "path to folder where graphs will be saved to"
+#' extractGraphs(sof, saf)
+#' @export
+extractGraphs = function(source_folder = getwd(), save_folder = getwd()){
+  registerDoParallel(7)
+  setwd(source_folder)
+  filenames = as.list(list.files(source_folder, pattern = ".png"))
+  
+  foreach(i = 1:length(filenames)) %dopar%{
+    path = paste0(source_folder,"/", filenames[[i]])
+    writer_id = paste0(substr(basename(path), start = 0, stop = nchar(basename(path))-17))
+    
+    setwd(save_folder)
+    ifelse(!dir.exists(file.path(save_folder, writer_id)), dir.create(file.path(save_folder, writer_id)), FALSE)
+    graph_writer = paste0(save_folder, writer_id, "/")
+    
+    getGraphs(filenames[[i]], source_folder = source_folder, save_folder = graph_writer)
+  }
+}
+
+
+sf = "/lss/research/csafe-handwriting/amymc/demo/data/source_test"
+gf = "/lss/research/csafe-handwriting/Data_Processing/graph_test/"
+cf = "/lss/research/csafe-handwriting/Data_Processing/clust_test/"
+filenames = list.files(sf, pattern="*.png", full.names=TRUE)
+extractGraphs(sf, gf)
