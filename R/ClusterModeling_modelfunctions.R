e49373403815ff5b24999097eabf1796df42003e
Stephanie Reinders <reinders.stephanie@gmail.com>
2022-10-05 09:44:17 -0500

    Update fit_model() input from model_data$rjags to model_data.
    I think the input model_data is more intuitive than model_data$rjags. I updated fit_model() and the tests and examples.

--- R/ClusterModeling_modelfunctions.R
+++ R/ClusterModeling_modelfunctions.R
@@ -3,21 +3,23 @@
 #' `fit_model()` fits a Bayesian hierarchical model to `model_training_data` and
 #' draws samples from the model with MCMC.
 #'
-#' @param model_training_data A list of input data formatted with
+#' @param model_data A list of input data formatted with
 #'   `format_model_data` for rjags
 #' @param num_iters An integer number of iterations of MCMC.
 #' @return A list of data frames of MCMC draws.
 #'
 #' @examples
-#' draws <- fit_model(example_model_data$rjags_data, 1000)
+#' draws <- fit_model(model_data = example_model_data, num_iters = 1000)
 #'
 #' @keywords model
 #'
 #' @export
 #' @md
-fit_model <- function(model_training_data, num_iters) {
+fit_model <- function(model_data, num_iters) {
+  rjags_data <- model_data$rjags_data
+  
   # fit model with rjags
-  m <- rjags::jags.model(textConnection(model_wrapped_cauchy), data = model_training_data, n.chains = 1)
+  m <- rjags::jags.model(textConnection(model_wrapped_cauchy), data = rjags_data, n.chains = 1)
 
   # mcmc draws
   fit <- rjags::coda.samples(m, c("theta", "gamma", "mu", "rho", "eta", "nll_datamodel", "nld_locationparam"), n.iter = num_iters)
@@ -46,7 +48,7 @@ fit_model <- function(model_training_data, num_iters) {
 #' @return A list of data frames of MCMC draws with burn-in dropped.
 #'
 #' @examples
-#' draws <- fit_model(example_model_data$rjags_data, 4000)
+#' draws <- fit_model(model_data = example_model_data, num_iters = 4000)
 #' draws <- drop_burnin(draws, 1000)
 #'
 #' @keywords model
@@ -110,7 +112,7 @@ model {
 #' writership for the questioned documents using MCMC draws from a hierarchical
 #' model created with `fit_model()`.
 #'
-#' @param model_training_data A list of input data formatted with
+#' @param model_data A list of input data formatted with
 #'   `format_model_data` for rjags
 #' @param draws MCMC draws created with `fit_model()`
 #' @param questioned_data A list questioned documents's data formatted with
@@ -122,7 +124,7 @@ model {
 #'
 #' @examples
 #' \dontrun{
-#' draws <- fit_model(model_training_data = example_model_data$rjags_data, num_iters = 4000)
+#' draws <- fit_model(model_data = example_model_data, num_iters = 4000)
 #' draws <- drop_burnin(draws, burn_in = 1000)
 #' draws <- analyze_questioned_documents(example_model_data,
 #'                                       draws,
@@ -134,27 +136,29 @@ model {
 #'
 #' @export
 #' @md
-analyze_questioned_documents <- function(model_training_data, draws, questioned_data, num_cores) {
-
+analyze_questioned_documents <- function(model_data, draws, questioned_data, num_cores) {
+  
+  rjags_data <- model_data$rjags_data
+  
   # initialize
   niter <- nrow(draws$thetas)
-  thetas <- array(dim = c(niter, model_training_data$rjags_data$G, model_training_data$rjags_data$W)) # 3 dim array, a row for each mcmc iter, a column for each cluster, and a layer for each writer
-  mus <- rhos <- array(dim = c(niter, model_training_data$rjags_data$Gsmall, model_training_data$rjags_data$W)) # 3 dim array, a row for each mcmc iter, a column for each cluster, and a layer for each writer
-  dmult <- dwc_sums <- data.frame(matrix(nrow = niter, ncol = model_training_data$rjags_data$W))
-  layered_wc_params <- array(dim = c(niter, model_training_data$rjags_data$Gsmall, 2))
+  thetas <- array(dim = c(niter, rjags_data$G, rjags_data$W)) # 3 dim array, a row for each mcmc iter, a column for each cluster, and a layer for each writer
+  mus <- rhos <- array(dim = c(niter, rjags_data$Gsmall, rjags_data$W)) # 3 dim array, a row for each mcmc iter, a column for each cluster, and a layer for each writer
+  dmult <- dwc_sums <- data.frame(matrix(nrow = niter, ncol = rjags_data$W))
+  layered_wc_params <- array(dim = c(niter, rjags_data$Gsmall, 2))
   ls <- list()
 
   # reshape variables
   flat_theta <- as.data.frame(cbind(iters = 1:niter, draws$thetas))
   flat_mus <- as.data.frame(cbind(iters = 1:niter, draws$mus))
   flat_rhos <- as.data.frame(cbind(iters = 1:niter, draws$rhos))
-  for (i in 1:model_training_data$rjags_data$W) { # i is writer, j is graph
-    for (j in 1:model_training_data$rjags_data$G) {
+  for (i in 1:rjags_data$W) { # i is writer, j is graph
+    for (j in 1:rjags_data$G) {
       thetas[, j, i] <- flat_theta[1:niter, as.character(paste0("theta[", i, ",", j, "]"))]
     }
   }
-  for (i in 1:model_training_data$rjags_data$W) { # i is writer, j is graph
-    for (j in 1:model_training_data$rjags_data$Gsmall) {
+  for (i in 1:rjags_data$W) { # i is writer, j is graph
+    for (j in 1:rjags_data$Gsmall) {
       mus[, j, i] <- flat_mus[1:niter, as.character(paste0("mu[", i, ",", j, "]"))]
       rhos[, j, i] <- flat_rhos[1:niter, as.character(paste0("rho[", i, ",", j, "]"))]
     }
@@ -176,7 +180,7 @@ analyze_questioned_documents <- function(model_training_data, draws, questioned_
     m_pcrot <- circular::circular(m_qdoc$pc_wrapped, units = "radians", modulo = "2pi")
 
     if (length(m_cluster) > 0) {
-      for (i in 1:model_training_data$rjags_data$W) { # i is writer, j is graph
+      for (i in 1:rjags_data$W) { # i is writer, j is graph
         dmult[, i] <- mc2d::dmultinomial(x = questioned_data$cluster_fill_counts[m, -c(1, 2)], prob = thetas[, , i], log = TRUE)
         layered_wc_params[, , 1] <- mus[, , i]
         layered_wc_params[, , 2] <- rhos[, , i]
@@ -184,7 +188,7 @@ analyze_questioned_documents <- function(model_training_data, draws, questioned_
       }
       nn <- dmult + dwc_sums + abs(max(colMeans(dmult + dwc_sums)))
     } else if (length(m_cluster) == 0) {
-      for (i in 1:model_training_data$rjags_data$W) { # i is writer, j is graph
+      for (i in 1:rjags_data$W) { # i is writer, j is graph
         dmult[, i] <- dmultinomial(x = questioned_data$cluster_fill_counts[m, -c(1, 2)], prob = thetas[, , i], log = TRUE)
       }
       nn <- dmult + abs(max(colMeans(dmult)))
