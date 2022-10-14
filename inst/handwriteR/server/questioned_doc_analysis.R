3697c547a4028184ac2b6bd803b2f8f0c5efd2fd
Stephanie Reinders <reinders.stephanie@gmail.com>
2022-10-14 14:23:44 -0500

    Add button to load template(s) to Shiny app.
    I added a button so that users can load template(s) that they have alreaady created.

--- inst/handwriteR/server/questioned_doc_analysis.R
+++ inst/handwriteR/server/questioned_doc_analysis.R
@@ -4,7 +4,16 @@
 
 analysis <- reactiveValues(q_main_dir = "/Users/stephanie/Documents/shiny_example",
                            q_template_images_dir = "/Users/stephanie/Documents/shiny_example/data/template_images",
-                           q_template_graphs_dir = "/Users/stephanie/Documents/shiny_example/data/template_graphs")
+                           q_template_graphs_dir = "/Users/stephanie/Documents/shiny_example/data/template_graphs",
+                           q_file = NULL,
+                           q_templates = NULL)
+
+# UPLOAD: templates
+observeEvent(input$q_load_templates, {
+  file <- input$q_load_templates
+  analysis$q_file <- file$datapath
+  analysis$q_templates <- readRDS(analysis$q_file)
+})
 
 # UPDATE:
 observe({
@@ -23,7 +32,7 @@ observeEvent(input$q_make_templates, {
   analysis$q_template_graphs_docnames <- list.files(analysis$q_template_graphs_dir)
   
   # make templates
-  analysis$templates <- make_clustering_templates(template_dir = analysis$q_main_dir,
+  analysis$q_templates <- make_clustering_templates(template_dir = analysis$q_main_dir,
                                                   writer_indices = c(2,5),
                                                   max_edges = 30,
                                                   starting_seed = input$q_starting_seed,
@@ -42,9 +51,11 @@ observeEvent(input$q_make_templates, {
 # RENDER:
 output$q_main_dir <- renderText({ analysis$q_main_dir })
 
+# template images
 output$q_template_images_dir <- renderText({ analysis$q_template_images_dir })
 output$q_template_images_docnames <- renderPrint({ list.files(analysis$q_template_images_dir) })
 
+# template graphs
 output$q_template_graphs_dir <- renderPrint({ analysis$q_template_graphs_dir })
 output$q_template_graphs_docnames <- renderPrint({ 
   if (is.null(analysis$q_template_graphs_docnames)){
@@ -53,4 +64,9 @@ output$q_template_graphs_docnames <- renderPrint({
     analysis$q_template_graphs_docnames
   }
 })
+
+# templates
+output$q_templates <- renderPrint({ length(analysis$q_templates) })
+
+
 
