d53a80328d722f48bd43a309c3f07b34efb93e48
Stephanie Reinders <reinders.stephanie@gmail.com>
2022-11-04 09:19:47 -0500

    Cleaned up several files/folders from examples folder for issue #23
    Deleted folder examples/50CSAFE_50CVL_2022-06-17. This folder contained an example cluster template. Now that `example_cluster_template` lives in the data folder this folder is no longer needed.
    
    Moved the code in generate_example_data.R to data-raw > example_data.R per instructions in section 8.2.1 of https://r-pkgs.org/data.html. data-raw was added to .Rbuildignore
    
    Moved csafeExample.R to the vignettes folder because it appears to give examples of how to perform basic processing steps.

---
+++ data-raw/example_data.R
@@ -0,0 +1,86 @@
+## code to prepare `example_data`
+
+# The functions in this script create `example_cluster_template`, `example_model_1chain`, 
+# `example_model_2chains`, `example_analysis_1chain`, and `example_analysis_2chains` and
+# save them in the data folder as rda files.
+
+# Creating these data objects requires saving processed handwriting files, template helper files,
+# and other assorted files to a folder on the local computer. Specify which folder you would 
+# like to use for this task with `main_dir`. The `main_dir` folder may be deleted after the 
+# example data has been saved to the data folder.
+
+# All of the functions below are set to overwrite example data files currently in the data folder.
+
+# choose main directory ----
+# path to main directory
+main_dir <- "/Users/stephanie/Documents/non_version_control/CSAFE_example_data_new"
+
+# choose starting seed and run number
+seed <- 100
+
+
+make_example_template <- function(main_dir, seed) {
+  # create folder if it doesn't already exist
+  if (!dir.exists(main_dir)){dir.create(main_dir)}
+  
+  template_images_dir <- system.file("extdata/example_images/template_training_images", 
+                                     package = "handwriter")
+  
+  example_cluster_template <- make_clustering_templates(template_dir = main_dir,
+                                                        template_images_dir = template_images_dir,
+                                                        writer_indices = c(2,5),
+                                                        max_edges = 30,
+                                                        K = 10,
+                                                        num_dist_cores = 2,
+                                                        max_iters = 3,
+                                                        num_graphs = 1000,
+                                                        seed = seed)
+  
+  usethis::use_data(example_cluster_template, overwrite = TRUE)
+}
+
+
+make_example_models <- function(main_dir){
+  model_images_dir <- system.file("extdata/example_images/model_training_images", 
+                                  package = "handwriter")
+  example_model_1chain <- fit_model(template_dir = main_dir, 
+                                    model_images_dir = model_images_dir,
+                                    num_iters = 100, 
+                                    num_chains = 1, 
+                                    writer_indices = c(2,5), 
+                                    doc_indices = c(7,18))
+  usethis::use_data(example_model_1chain, overwrite = TRUE)
+  
+  example_model_2chains <- fit_model(template_dir = main_dir, 
+                                     model_images_dir = model_images_dir,
+                                     num_iters = 100, 
+                                     num_chains = 2, 
+                                     writer_indices = c(2,5), 
+                                     doc_indices = c(7,18))
+  usethis::use_data(example_model_2chains, overwrite = TRUE)
+}
+
+
+make_example_analyses <- function(main_dir, num_cores = 5) {
+  questioned_images_dir <- system.file("extdata/example_images/questioned_images", 
+                                       package = "handwriter")
+  
+  example_analysis_1chain <- analyze_questioned_documents(template_dir = main_dir, 
+                                                          questioned_images_dir = questioned_images_dir, 
+                                                          model = example_model_1chain, 
+                                                          num_cores = num_cores)
+  
+  usethis::use_data(example_analysis_1chain, overwrite = TRUE)
+  
+  example_analysis_2chains <- analyze_questioned_documents(template_dir = main_dir, 
+                                                           questioned_images_dir = questioned_images_dir, 
+                                                           model = example_model_2chains, 
+                                                           num_cores = num_cores)
+  
+  usethis::use_data(example_analysis_2chains, overwrite = TRUE)
+}
+
+
+make_example_template(main_dir, seed)
+make_example_models(main_dir)
+make_example_analyses(main_dir)
