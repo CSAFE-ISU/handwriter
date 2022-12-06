e6e79a07b17fcabaf1102c1764cf3ebf87e912c7
Stephanie Reinders <reinders.stephanie@gmail.com>
2022-12-06 09:58:34 -0600

    Rebuilt csafe_docs data object to include new docs.
    I first built csafe_docs only using version 2 of the CSAFE Handwriting Data Portal. I rebuilt it to use version 3.

--- data-raw/csafe_docs.R
+++ data-raw/csafe_docs.R
@@ -2,7 +2,30 @@
 # that all CSAFE documents be saved in the input_dir. Do not rename any of the 
 # documents.
 
-input_dir <- "~/Documents/non_version_control/CSAFE/data_portal/downloaded"
-csafe_docs = data.frame(doc = list.files(input_dir, pattern = ".png"))
-csafe_docs = csafe_docs %>% tidyr::separate(doc, into=c("writer", "session", "prompt", "repetition"), extra="drop", remove = FALSE)
+input_dir <- "/Volumes/lss/research/csafe-handwriting/Data_Processing/Stage4_Cropped/Writing/Complete"
+
+# list writer folders
+writers <- dir(input_dir, recursive = FALSE, full.names = TRUE)
+writers <- writers[grepl("w\\d+", writers)]  # remove any non-writer folders
+
+# list docs
+docs <- unlist(sapply(writers, function(x) list.files(x, pattern = ".png"), USE.NAMES = FALSE))
+
+# make data frame
+csafe_docs <- data.frame(doc = docs)
+csafe_docs = csafe_docs %>% 
+  tidyr::separate(doc, into=c("writer", "session", "prompt", "repetition"), extra="drop", remove = FALSE)
+
+# make reps 
+csafe_docs <- csafe_docs %>% 
+  dplyr::mutate(doc = stringr::str_replace_all(doc, "r1", "r01"),
+                doc = stringr::str_replace_all(doc, "r2", "r02"),
+                doc = stringr::str_replace_all(doc, "r3", "r03"))
+
+csafe_docs <- csafe_docs %>% 
+  dplyr::mutate(repetition = stringr::str_replace_all(repetition, "r1", "r01"),
+                repetition = stringr::str_replace_all(repetition, "r2", "r02"),
+                repetition = stringr::str_replace_all(repetition, "r3", "r03"))
+
+# save to data folder
 usethis::use_data(csafe_docs, overwrite = TRUE)
