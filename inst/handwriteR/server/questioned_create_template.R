fe99882f8f460d4c2a237b355367fe6b2705b911
Stephanie Reinders <reinders.stephanie@gmail.com>
2023-03-08 09:45:37 -0600

    Added template output directory

--- inst/handwriteR/server/questioned_create_template.R
+++ inst/handwriteR/server/questioned_create_template.R
@@ -1,2 +1,4 @@
-dir_path <- directoryServer("t_docs")
-directoryContentsServer("t_docs_list", dir_path)
+t_docs_path <- directoryServer("t_docs")
+directoryContentsServer("t_docs_list", t_docs_path)
+
+t_output_path <- directoryServer("t_output")
--- inst/handwriteR/ui/questioned_create_template.R
+++ inst/handwriteR/ui/questioned_create_template.R
@@ -1,5 +1,9 @@
 tabPanel("Template",
          h2("Create a New Tempalte"),
          directoryUI("t_docs", label = "Template Training Documents"),
-         directoryContentsUI("t_docs_list")
+         directoryContentsUI("t_docs_list"),
+         hr(),
+         directoryUI("t_output", label = "Output Directory"),
+         helpText("The template and other related files with be saved in Output Directory > data."),
+         hr(),
          )
