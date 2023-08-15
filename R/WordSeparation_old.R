897ff1f761cb5db2ee30eb6ac261bd45266734b9
Stephanie Reinders <reinders.stephanie@gmail.com>
2023-08-15 10:00:44 -0500

    Forgot to commit

--- R/WordSeparation_old.R
+++ R/WordSeparation_old.R
@@ -1,3 +1,8 @@
+library(rjson)
+library(randomForest)
+library(usethis)
+
+
 #' add_word_info
 #'
 #' Associates characters to their respective word numbers by ML on labeled data
@@ -305,6 +310,93 @@ make_single_word = function(letterList){
   return(letterList)
 }
 
+#' makeModel
+#'
+#' Creates a randomForest word model
+#' 
+#' @param TaggedJson Json File with tagged letter data
+#' @return randomForest model
+#' 
+#' @importFrom randomForest randomForest
+makeModel = function(TaggedJson){
+  
+  na.exclude <- NULL
+  
+  #Load JSON file
+  AsJSON <- rjson::fromJSON(file = TaggedJson)
+  
+  #Create a new DF and fill it up from each character entry
+  dataDF <- data.frame(line=numeric(0),line_height=numeric(0),line_width=numeric(0),height=numeric(0),width=numeric(0),x=numeric(0),label=character(0))
+  for (i in 1:length(AsJSON)){
+    for(char in AsJSON[[i]][["annotations"]][[1]][["result"]]){
+      dataDF[nrow(dataDF) + 1,] =
+        list(i,
+             char$original_height,char$original_width,
+             char$value$height,char$value$width,
+             char$value$x,
+             char$value$rectanglelabels)
+    }
+  }
+  
+  #Sort DF By first line number, and then x value
+  dataDF=dataDF[order( dataDF[,1], dataDF[,6] ),]
+  #print(nrow(dataDF))
+  
+  #Add proportionalized data
+  dataDF['height_prop'] <- NA
+  dataDF['width_prop'] <- NA
+  dataDF['to_right_prop'] <- NA
+  dataDF['to_left_prop'] <- NA
+  
+  for(r in 1:nrow(dataDF)){
+    row = dataDF[r,]
+    prev_row = dataDF[r-1,]
+    next_row = dataDF[r+1,]
+    
+    dataDF[r, 'height_prop'] = row$height/row$line_height
+    dataDF[r, 'width_prop'] = row$width/row$line_width
+    
+    to_right = next_row$x - (row$x + row$width)
+    dataDF[r, 'to_right_prop'] = to_right/row$line_width
+    
+    if(r==1){next}
+    to_left = row$x - (prev_row$x + prev_row$width)
+    dataDF[r, 'to_left_prop'] = to_left/row$line_width
+  } 
+  
+  dataDF = pmax(dataDF, 0)
+  
+  trainDF = dataDF[c("label", "height_prop", "width_prop", "to_right_prop", "to_left_prop")]
+  aggregateDataDF = aggregate(. ~ label, trainDF, mean)
+  print(aggregateDataDF)
+  
+  #Reconfigure DF for to be put into model
+  trainDF$label = factor(trainDF$label)
+  
+  #Train a model
+  train <- sample(nrow(trainDF), 0.7*nrow(trainDF), replace = FALSE)
+  TrainSet <- trainDF[train,]
+  ValidSet <- trainDF[-train,]
+  summary(TrainSet)
+  summary(ValidSet)
+  
+  #Model Testing
+  a=c()
+  for (i in 1:4) {
+    model1 <- randomForest(label ~ ., data = TrainSet, ntree = 500, mtry = i, importance = TRUE, na.action=na.exclude)
+    predValid <- predict(model1, ValidSet, type = "class")
+    a[i] = mean(predValid == ValidSet$label, na.rm = TRUE)
+  }
+  
+  
+  print(a)
+  
+  #Now that model is trained, save it so it can be loaded
+  wordModelNew = NULL
+  wordModelNew <- randomForest(label ~ ., data = TrainSet, ntree = 500, mtry = 4, importance = TRUE, na.action=na.exclude)
+  usethis::use_data(wordModelNew, overwrite = TRUE)
+}
+
 #' plotWord
 #'
 #' This function returns a plot of a single Word extracted from a document. It uses the letterList parameter from the processHandwriting function and accepts a single value as whichLetter. Dims requires the dimensions of the entire document, since this isn't contained in processHandwriting.
@@ -428,3 +520,12 @@ process_words = function(words, dims, triangulate = FALSE){
   
   return(words);
 }
+
+#' wordModelNew is the RandomForest model to decide if a word separation has happened
+#' 
+#'
+#' @name wordModelNew
+#' @docType data
+#' @keywords data
+NULL
+
