aec059b92e32ba9719419836df360ade2219b427
Stephanie Reinders <reinders.stephanie@gmail.com>
2024-03-08 09:53:16 -0600

    Deleted model_wrapped_cauchy from data folder to reduce packaage size.

--- data-raw/example_data.R
+++ data-raw/example_data.R
@@ -1,7 +1,7 @@
 ## code to prepare `example_data`
 
 # The functions in this script create `example_cluster_template`,
-# `model_wrapped_cauchy`, `example_model_1chain`, `example_model_2chains`,
+# `example_model_1chain`, `example_model_2chains`,
 # `example_analysis_1chain`, and `example_analysis_2chains` and save them in the
 # data folder as rda files.
 
@@ -34,56 +34,6 @@ make_example_template <- function(main_dir, centers_seed, graphs_seed) {
   # save to data folder
   usethis::use_data(example_cluster_template, overwrite = TRUE)
 }
-#' Wrapped-Cauchy Stan model code.
-#'
-#' This object contains the Stan model code for the wrapped-Cauchy model.
-#'
-#' @name model_wrapped_cauchy
-#' @rdname model_wrapped_cauchy
-make_model_wrapped_cauchy <- function(){
-  model_wrapped_cauchy <- "
-model {
-  for(letter in 1:numletters){
-    # numletters = num unique letters with measurements
-    # use zeros trick to sample from wrapped-Cauchy distribution
-    nll_datamodel[letter] = -log( (1-pow(tau[letterwriter[letter], lettercluster[letter]],2)) / (2*pi_constant*(1+pow(tau[letterwriter[letter], lettercluster[letter]],2)-2*tau[letterwriter[letter], lettercluster[letter]]*cos(pc_wrapped[letter]-mu[letterwriter[letter], lettercluster[letter]]))) ) + C
-    zero_vec[letter] ~ dpois( nll_datamodel[letter] )
-  }
-
-  # Priors for wrapped cauchy
-  for(g in 1:Gsmall){
-    # g = cluster
-    gamma[g] ~ dgamma(a, b)
-    eta[g] ~ dunif(0,2*pi_constant)
-    for(w in 1:W){
-      # W = num unique writers
-      # use zeros trick to sample from wrapped-Cauchy distribution
-      mu[w,g]  ~ dunif(0,2*pi_constant)
-      nld_locationparam[w,g] = -log( (1-pow(e,2)) / (2*pi_constant*(1+pow(e,2)-2*e*cos(mu[w,g]-eta[g]))) ) + C
-      zero_mat[w,g] ~ dpois(nld_locationparam[w,g])
-      tau[w,g] ~ dbeta(c,d)
-    }
-  }
-
-  for (w in 1:W) {
-    # w = writer
-    pi[w,1:G] ~ ddirch(gamma[1:G] + 0.001)
-  }
-
-  for(d in 1:D) {
-    # d = document
-    Y[d,1:G] ~ dmulti(pi[docwriter[d],1:G], docN[d])
-  }
-
-  # other values
-  C = 30   # for the zeros trick
-  pi_constant = 3.14159
-  pi_1 = -pi_constant
-}"
-  
-  usethis::use_data(model_wrapped_cauchy, overwrite = TRUE)
-}
-
 
 make_example_models <- function(main_dir){
   model_docs <- system.file("extdata/example_images/model_docs", 
@@ -146,7 +96,6 @@ graphs_seed <- 104
 
 # make example template
 make_example_template(main_dir, centers_seed, graphs_seed)
-make_model_wrapped_cauchy()
 
 # make example models using the new example template
 devtools::load_all()
