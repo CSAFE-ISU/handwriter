7af36d81ddefcafb0685a8f781b830829e590224
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-02-06 10:33:22 -0600

    Fixed test for `get_clusters_batch()`
    Test mistakenly expected a message in a situation where the function should not produce a message.

--- tests/testthat/test-cluster-assignment.R
+++ tests/testthat/test-cluster-assignment.R
@@ -85,12 +85,12 @@ test_that("get_clusters_batch can save master file ", {
   empty_tempdir("clusters")
   
   results <- get_clusters_batch(example_cluster_template,
-                               testthat::test_path("fixtures", "temp1qd", "data", "model_graphs"),
-                               file.path(tempdir(), "clusters"),
-                               writer_indices = c(1, 5),
-                               doc_indices = c(7, 18),
-                               num_cores = 1,
-                               save_master_file = TRUE)
+                                testthat::test_path("fixtures", "temp1qd", "data", "model_graphs"),
+                                file.path(tempdir(), "clusters"),
+                                writer_indices = c(1, 5),
+                                doc_indices = c(7, 18),
+                                num_cores = 1,
+                                save_master_file = TRUE)
   actual <- readRDS(file.path(tempdir(), "clusters", "all_clusters.rds"))
   expected <- readRDS(testthat::test_path("fixtures", "temp1qd", "data", "model_clusters.rds"))
   # TODO: Why aren't pc_rotation and pc_wrapped values exactly the same?
@@ -173,11 +173,11 @@ testthat::test_that("get_clusters_batch running in parallel moves problem file t
   create_dir(file.path(tempdir(), "graphs"))
   saveRDS(doc, file.path(tempdir(), "graphs", "w0009_s01_pWOZ_r01_proclist.rds"))
   
-  testthat::expect_message(get_clusters_batch(
+  output <- get_clusters_batch(
     template = example_cluster_template,
     input_dir = file.path(tempdir(), "graphs"),
     output_dir = file.path(tempdir(), "clusters"),
-    num_cores = 2))
+    num_cores = 2)
   
   # Check that file was moved to problem_files folder
   testthat::expect_true(file.exists(file.path(tempdir(), "clusters", "problem_files", "w0009_s01_pWOZ_r01_proclist.rds")))
@@ -218,4 +218,82 @@ testthat::test_that("get_clusters_batch returns NULL if file doesn't contain gra
     output_dir = file.path(tempdir(), "clusters"),
     num_cores = 2)
   testthat::expect_null(actual)
+})
+
+
+# test get_clusterassignment ----------------------------------------------
+
+testthat::test_that("get_clusterassignment returns an error if input_type is not 'model' or 'questioned' ", {
+  empty_tempdir("clusters")
+  
+  testthat::expect_error(
+    get_clusterassignment(
+      main_dir = file.path(tempdir(), "clusters"),
+      input_type = "template",
+      writer_indices = c(1, 5),
+      doc_indices = c(7, 18),
+      num_cores = 1
+    ),
+    "Unknown input type. Use model or questioned."
+  )
+})
+
+testthat::test_that("get_clusterassignment loads clusters file if it already exists", {
+  empty_tempdir("main_dir")
+  
+  create_dir(file.path(tempdir(), "main_dir", "data"), recursive = TRUE)
+  # copy model_clusters.rds to tempdir
+  file.copy(
+    testthat::test_path("fixtures", "temp1qd", "data", "model_clusters.rds"),
+    file.path(tempdir(), "main_dir", "data", "model_clusters.rds")
+  )
+  
+  actual <- get_clusterassignment(
+    main_dir = file.path(tempdir(), "main_dir"),
+    input_type = "model",
+    writer_indices = c(1, 5),
+    doc_indices = c(7, 18),
+    num_cores = 1
+  )
+  
+  expected <- readRDS(testthat::test_path("fixtures", "temp1qd", "data", "model_clusters.rds"))
+  
+  testthat::expect_identical(actual, expected)
+})
+
+testthat::test_that("get_clusterassignment throws an error if template.rds is not in data folder in the main directory", {
+  empty_tempdir("main_dir")
+  
+  testthat::expect_error(
+    get_clusterassignment(
+      main_dir = file.path(tempdir(), "main_dir"),
+      input_type = "model",
+      writer_indices = c(1, 5),
+      doc_indices = c(7, 18),
+      num_cores = 1
+    ),
+    paste0("There is no cluster template in ", file.path(tempdir(), "main_dir"))
+  )
+})
+
+testthat::test_that("get_clusterassignment creates the output directory if it doesn't exist", {
+  empty_tempdir("main_dir")
+  
+  # copy graph files to tempdir
+  copy_files(
+    input_dir = testthat::test_path("fixtures", "temp1qd", "data", "questioned_graphs"),
+    output_dir = file.path(tempdir(), "main_dir", "data", "questioned_graphs")
+  )
+  # save template in tempdir
+  saveRDS(example_cluster_template, file.path(tempdir(), "main_dir", "data", "template.rds"))
+  
+  testthat::expect_no_error(get_clusterassignment(
+      main_dir = file.path(tempdir(), "main_dir"),
+      input_type = "questioned",
+      writer_indices = c(1, 5),
+      doc_indices = c(7, 18),
+      num_cores = 1
+  ))
+  
+  testthat::expect_true(dir.exists(file.path(tempdir(), "main_dir", "data", "questioned_clusters")))
 })
