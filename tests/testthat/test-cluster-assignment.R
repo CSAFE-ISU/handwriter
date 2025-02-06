4aee73849e5f8c5928b3e92f07b2eac26bbfbf76
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-02-06 09:40:26 -0600

    Added tests for `get_clusters_batch()`
    - Added a test to check that `get_clusters_batch()` saves a master file of all cluster assignments if `save_master_file` is `TRUE`.
    - A test already existed to check that `get_clusters_batch()` moves a problem file to the problem_files folder if the function is running sequentially. Added a test to check this if the function is running in parallel.

--- tests/testthat/test-cluster-assignment.R
+++ tests/testthat/test-cluster-assignment.R
@@ -81,6 +81,22 @@ test_that("get_clusters_batch works sequentially without writer or doc indices",
   expect_equal(actual, expected, tolerance = 10^-3)
 })
 
+test_that("get_clusters_batch can save master file ", {
+  empty_tempdir("clusters")
+  
+  results <- get_clusters_batch(example_cluster_template,
+                               testthat::test_path("fixtures", "temp1qd", "data", "model_graphs"),
+                               file.path(tempdir(), "clusters"),
+                               writer_indices = c(1, 5),
+                               doc_indices = c(7, 18),
+                               num_cores = 1,
+                               save_master_file = TRUE)
+  actual <- readRDS(file.path(tempdir(), "clusters", "all_clusters.rds"))
+  expected <- readRDS(testthat::test_path("fixtures", "temp1qd", "data", "model_clusters.rds"))
+  # TODO: Why aren't pc_rotation and pc_wrapped values exactly the same?
+  expect_equal(actual, expected, tolerance = 10^-3)
+})
+
 test_that("get_clusters_batch works in parallel without writer or doc indices", {
   empty_tempdir("clusters")
   
@@ -126,7 +142,7 @@ testthat::test_that("get_clusters_batch throws expected errors", {
     "num_cores is not greater than or equal to 1")
 })
 
-testthat::test_that("get_clusters_batch moves problem file problem_files folder", {
+testthat::test_that("get_clusters_batch running sequentially moves problem file to problem_files folder", {
   empty_tempdir("graphs")
   empty_tempdir("clusters")
   
@@ -146,6 +162,28 @@ testthat::test_that("get_clusters_batch moves problem file problem_files folder"
   testthat::expect_true(file.exists(file.path(tempdir(), "clusters", "problem_files", "w0009_s01_pWOZ_r01_proclist.rds")))
   testthat::expect_false(file.exists(file.path(tempdir(), "graphs", "w0009_s01_pWOZ_r01_proclist.rds")))
 })
+
+testthat::test_that("get_clusters_batch running in parallel moves problem file to problem_files folder", {
+  empty_tempdir("graphs")
+  empty_tempdir("clusters")
+  
+  # Make a graphs file without a docname
+  doc <- readRDS(testthat::test_path("fixtures", "temp1qd", "data", "model_graphs", "w0009_s01_pWOZ_r01_proclist.rds"))
+  doc$docname <- NULL
+  create_dir(file.path(tempdir(), "graphs"))
+  saveRDS(doc, file.path(tempdir(), "graphs", "w0009_s01_pWOZ_r01_proclist.rds"))
+  
+  testthat::expect_message(get_clusters_batch(
+    template = example_cluster_template,
+    input_dir = file.path(tempdir(), "graphs"),
+    output_dir = file.path(tempdir(), "clusters"),
+    num_cores = 2))
+  
+  # Check that file was moved to problem_files folder
+  testthat::expect_true(file.exists(file.path(tempdir(), "clusters", "problem_files", "w0009_s01_pWOZ_r01_proclist.rds")))
+  testthat::expect_false(file.exists(file.path(tempdir(), "graphs", "w0009_s01_pWOZ_r01_proclist.rds")))
+})
+
 
 testthat::test_that("get_clusters_batch throws warning if file doesn't contain graphs and running sequentially", {
   empty_tempdir("graphs")
